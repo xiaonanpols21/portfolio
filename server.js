826b0f93be7df1feec5eb4f49b2339bc883e79d4
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-06-13 11:22:28 +0200

    mappen structuur backend

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render("pages/index");
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
