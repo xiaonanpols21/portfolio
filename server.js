ba3722d5099ef5441c6b019fb04bedf6fda4b02d
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-06-14 10:08:01 +0200

    mongo ok

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const express = require('express');
 
+const dotenv = require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const { ObjectId } =   require("mongodb");
+
 const app = express();
 const port = 3000;
+
+let db = null;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render("pages/index");
+app.get('/', async (req, res) => {
+    const skills = await db.collection("skills").find({},{}).toArray();
+
+    res.render("pages/index", {
+        skills
+    });
 })
+
+// Make connection with Mongo
+async function connectDB() {
+    const uri = process.env.DB_URI;
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    try {
+        await client.connect();
+        db = client.db(process.env.DB_NAME);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
+    connectDB()
+    .then(console.log("We have a connection to mongo"));
 });
