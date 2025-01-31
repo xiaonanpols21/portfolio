96b434626d4a8e8cb06a70f95a51e31436dd2510
xiaonanpols21 <xiaonanpolspols@gmail.com>
2025-01-31 14:57:28 +0100

    css files routing verbeteren

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,31 @@
+import Link from "next/link";
+import Header from "@/components/header";
+import Hero from "@/components/pages/home/sections/hero";
+import Experiences from "@/components/pages/home/sections/experiences";
+import Contact from "@/components/pages/home/sections/contact";
+import Footer from "@/components/footer";
+import styles from "./portfolio.module.scss";
+
 export default function Home() {
   return (
-    <h1>Xiao Design</h1>
+    <>
+        {/* Fixed content */}
+        <Link className={styles.skipBtn} href="#projecten">Ga naar projecten</Link>
+        <Header/>
+        
+        <Hero/>
+        <Experiences/>
+        <Contact/>
+        <Footer/>
+        
+        {/* Fixed content */}
+        {/* <BtnPrimaireShadow
+            aria="Mijn favorieten knop"
+            img={"../../img/portfolio/icons/bookmark-fill.svg"}
+            alt={"Check icoon"}
+            classes={styles.bookmark}
+        /> */}
+        {/* <Bookmarked/> */}
+    </>
   );
 }
---
+++ src/components/pages/home/sections/hero/index.js
@@ -0,0 +1,32 @@
+import Switch from "@/components/pages/home/sections/hero/switch";
+import LinkPrimaire from "@/components/links/primaire";
+import Projects from "@/components/pages/home/sections/hero/projects";
+import Tags from "@/components/tags/skills";
+import styles from "./hero.module.scss";
+
+export default function Hero() {
+    return (
+        <section className={styles.hero}>
+            <h4 className="visually-hidden">Hero</h4>
+            {/* <Switch/> */}
+
+            <section className={styles.content}>
+                <h4 className="visually-hidden">Header content</h4>
+                <div>
+                    <h1>Xiao NaN</h1>
+                    <p>Front-end developer</p>
+                    <Tags/>
+                    <p>Een creatieve front-end develop met oog voor design waarbij toegankelijkheid belangrijk is voor iedereen!</p>
+                    <LinkPrimaire
+                        link={"/portfolio/over-mij"}
+                        aria="Over mij knop"
+                        text={"Meer over mij"}
+                    />
+                </div>
+            </section>
+
+            <Projects/>
+            
+        </section>
+    )
+}
