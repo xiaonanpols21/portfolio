7509161f87424b0e4e23c512c20486a3b9a0da45
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2025-08-19 13:07:19 +0200

    delete clarity and att vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 import { Poppins, Wix_Madefor_Text } from "next/font/google";
-import Head from 'next/head';
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
-  weight: ['400', '700'], 
-  subsets: ['latin'],    
-  display: 'swap',       
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -17,21 +18,11 @@ import "../styles/index.scss";
 export default function RootLayout({ children }) {
   return (
     <html lang="nl">
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>{metadata.title}</title>
-            <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "sx8ndw6znz");
-</script>
-        </Head>
-        <body className={`${poppins.className}`}>
-            {children}
-        </body>
-
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </Head>
+      <body className={`${poppins.className}`}>{children}</body>
     </html>
   );
 }
